4916325a0b80e7d7734b64aacab6a7a10249c8cd
Rafi Barash <rafabarash@gmail.com>
2018-08-20 12:07:24 -0500

    hash practice

--- epi_judge_python/is_string_permutable_to_palindrome.py
+++ epi_judge_python/is_string_permutable_to_palindrome.py
@@ -1,9 +1,22 @@
 from test_framework import generic_test
+from collections import Counter
 
 
 def can_form_palindrome(s):
-    # TODO - you fill in here.
-    return True
+    # d = {}
+    # one_odd = False
+    # for ch in s:
+    #     if ch in d:
+    #         d[ch] += 1
+    #     else:
+    #         d[ch] = 1
+    # for val in d.values():
+    #     if val & 1:
+    #         if one_odd:
+    #             return False
+    #         one_odd = True
+    # return True
+    return sum(v % 2 for v in Counter(s).values()) <= 1
 
 
 if __name__ == '__main__':
